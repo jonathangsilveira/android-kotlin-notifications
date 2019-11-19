3ee055023c9f2ade787df906abbe3f1bd56d9e67
Jonathan Giorgi Silveira <jonathangsilveira@gmail.com>
2019-11-18 23:09:00 -0300

    Intent builder

--- app/src/main/java/com/example/android/eggtimernotifications/util/Intents.kt
+++ app/src/main/java/com/example/android/eggtimernotifications/util/Intents.kt
@@ -3,5 +3,5 @@ package com.example.android.eggtimernotifications.util
 import android.content.Context
 import android.content.Intent
 
-inline fun <reified T> intent(packageContext: Context, block: Intent.() -> Unit = {}): Intent =
-    Intent(packageContext, T::class.java).apply(block)
+inline fun <reified T> Context.newIntent(block: Intent.() -> Unit = {}): Intent =
+    Intent(this, T::class.java).apply(block)
